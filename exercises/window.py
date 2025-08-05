b6444d20529e195bc2ee5af1829ad4d9f83bc2af
lobomarcosemail <wofeworks@gmail.com>
2025-08-05 18:34:42 -0300

    ###

--- exercises/window.py
+++ exercises/window.py
@@ -1,6 +1,15 @@
 import tkinter as tk
 
+''' REDIMENSIONÁVEL
 janela = tk.Tk()
 janela.title ('Aplicação GUI')
+
+janela.mainloop()
+'''
+
+# NÃO REDIMENSIONÁVEL
+janela = tk.Tk()
+janela.title ('App GUI Não Redimensionável')
+janela.resizable (False, False)
 
 janela.mainloop()
